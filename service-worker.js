8da19cec3d335d8bf1d99bf6084ec8efc67aa353
mickhenderson-rgb <mick.henderson@deploytelco.com.au>
2026-08-08 23:48:28 +1000

    build-50 step 2

--- service-worker.js
+++ service-worker.js
@@ -32,7 +32,7 @@ Caching rules, deliberately conservative:
 =========================================================
 */
 
-const CACHE_NAME = "compass-tos-v25";
+const CACHE_NAME = "compass-tos-v26";
 
 const APP_SHELL = [
   "./",
